d8ac05e8d611bfd2192f2dc190dcd1fc3ecde38c
AtharvaThorve <aaathorve@gmail.com>
2021-04-12 16:20:07 +0530

    Simple logger program with channels

---
+++ src/Threads in Go/channels/logger.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	logInfo    = "INFO"
+	logWarning = "WARNING"
+	logError   = "ERROR"
+)
+
+type logEntry struct {
+	time     time.Time
+	severity string
+	message  string
+}
+
+var logCh = make(chan logEntry, 50)
+var doneCh = make(chan struct{}) // This is known as a signal only channel it cannot send or recieve data but it knows whether a message was send or not. Has zero memory allocation.
+
+func main() {
+	go logger()
+	logCh <- logEntry{time.Now(), logInfo, "App is starting"}
+
+	logCh <- logEntry{time.Now(), logInfo, "App is stopping"}
+	time.Sleep(time.Millisecond * 100)
+	doneCh <- struct{}{}
+}
+
+func logger() {
+	for {
+		select {
+		case entry := <-logCh:
+			fmt.Printf("%v - [%v]%v\n", entry.time.Format("2006-01-02T15:04:05"), entry.severity, entry.message)
+		case <-doneCh:
+			break
+			// default:
+			// 	fmt.Println("Non blocking select statement if default case is provided")
+		}
+	}
+}
